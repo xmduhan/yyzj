86ab2d9192538c408e08704584c01aeb66d6e152
xmduhan <xmduhan@gmail.com>
2018-10-11 16:33:21 +0800

    ....

--- gateway/__init__.py
+++ gateway/__init__.py
@@ -12,8 +12,6 @@ class JavaGateway(py4j.java_gateway.JavaGateway):
         """ """
         # path = os.path.realpath(__file__)
         path = os.path.dirname(os.path.realpath(__file__))
-        print(path)
-        print('{path}/libs/*:{path}'.format(**locals()))
         port = launch_gateway(classpath='{path}/libs/*:{path}'.format(**locals()))
         super(JavaGateway, self).__init__(gateway_parameters=GatewayParameters(port=port))
 
--- tests/test_lfasr_api.py
+++ tests/test_lfasr_api.py
@@ -1,25 +1,40 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import py4j
-from py4j.java_gateway import JavaGateway, GatewayParameters
 import json
+from time import sleep
 
-port = py4j.java_gateway.launch_gateway(classpath='jvm/libs/*:jvm')
-gateway = JavaGateway(gateway_parameters=GatewayParameters(port=port))
+from gateway import JavaGateway
 
-def test():
+
+def test_lfasr_api():
     """ """
+    # Load JVM
+    gateway = JavaGateway()
+    LfasrType = gateway.jvm.com.iflytek.msp.cpdb.lfasr.model.LfasrType
     LfasrClientImp = gateway.jvm.com.iflytek.msp.cpdb.lfasr.client.LfasrClientImp
     lc = LfasrClientImp.initLfasrClient()
-    LfasrType = gateway.jvm.com.iflytek.msp.cpdb.lfasr.model.LfasrType
 
+    # Upload wav file
     params = gateway.jvm.java.util.HashMap()
     params['has_participle'] = 'true'
+    # Upload = lc.lfasrUpload('test.wav', LfasrType.LFASR_STANDARD_RECORDED_AUDIO, params)
+    upload = lc.lfasrUpload('wav/test.wav', LfasrType.LFASR_TELEPHONY_RECORDED_AUDIO, params)
+    assert upload.getOk() == 0
+    task_id = upload.getData()
 
-    # upload_result = lc.lfasrUpload('test.wav', LfasrType.LFASR_STANDARD_RECORDED_AUDIO, params)
-    upload_result = lc.lfasrUpload('test.wav', LfasrType.LFASR_TELEPHONY_RECORDED_AUDIO, params)
-    upload_result.getOk()
-    task_id = upload_result.getData()
-    task_id
+    # Wait server
+    while True:
+        progress = lc.lfasrGetProgress(task_id)
+        assert progress.getOk() == 0
+        data = json.loads(progress.getData())
+        print(data)
+        if data['status'] == 9:
+            break
+        sleep(3)
 
+    # Read result
+    result = lc.lfasrGetResult(task_id)
+    assert result.getOk() == 0
+    data = json.loads(result.getData())
+    print(','.join([segment['onebest'] for segment in data]))
