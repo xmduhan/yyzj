a0551fc1d0507444fe8c049d50e977f08696c4cd
xmduhan <xmduhan@gmail.com>
2018-10-16 10:35:01 +0800

    ...

--- db/admin.py
+++ db/admin.py
@@ -6,6 +6,7 @@ from . import models
 
 class LfasrAdmin(admin.ModelAdmin):
     """ """
+    list_display = ['md5', 'filename', 'task_id', 'step', 'code', 'message', 'data']
 
 
 admin.site.register(models.LfasrModel, LfasrAdmin)
@@ -13,6 +14,7 @@ admin.site.register(models.LfasrModel, LfasrAdmin)
 
 class MailAdmin(admin.ModelAdmin):
     """ """
+    list_display = ['subject', 'date', 'mailfrom', 'message_id']
 
 
 admin.site.register(models.MailModel, MailAdmin)
