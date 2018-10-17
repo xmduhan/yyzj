cf768d6ad6a31c1bc4a2fc15beeb111956810db8
xmduhan <xmduhan@gmail.com>
2018-10-17 11:41:29 +0800

    ...

--- mail.py
+++ mail.py
@@ -2,10 +2,20 @@
 # encoding: utf-8
 
 import config
+
 import poplib
+import smtplib
 import chardet
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+import json
+import pandas as pd
+from collections import OrderedDict
+from datetime import datetime
 import shutil
 import environment
+from django.db.models import Q
 import lfasr
 from dateutil.parser import parse
 from email import message_from_bytes
@@ -67,41 +77,114 @@ def read_mail():
 
                     # Save attachment files
                     tempdir = tempfile.mkdtemp(dir='wav')
-                    for part in message.walk():
-                        if part.get_content_disposition() == 'attachment':
-                            filename = decode_header(part.get_filename())
-                            data = part.get_payload(decode=True)
-                            f = open(path.join(tempdir, filename), 'wb')
-                            f.write(data)
-                            f.close()
+                    try:
+                        for part in message.walk():
+                            if part.get_content_disposition() == 'attachment':
+                                filename = decode_header(part.get_filename())
+                                data = part.get_payload(decode=True)
+                                f = open(path.join(tempdir, filename), 'wb')
+                                f.write(data)
+                                f.close()
 
-                    # Extract zip files if exists
-                    for filename in glob(path.join(tempdir, '**/*.zip'), recursive=True):
-                        # print(filename, ''.join(filename.split('.')[:-1]))
-                        call(['unzip', filename, '-d', ''.join(filename.split('.')[:-1])])
-                        call(['rm', filename])
-                        print('Extract %s ...' % filename)
+                        # Extract zip files if exists
+                        for filename in glob(path.join(tempdir, '**/*.zip'), recursive=True):
+                            # print(filename, ''.join(filename.split('.')[:-1]))
+                            call(['unzip', filename, '-d', ''.join(filename.split('.')[:-1])])
+                            call(['rm', filename])
+                            print('Extract %s ...' % filename)
 
-                    # Upload all wav file
-                    for filename in glob(path.join(tempdir, '**/*.wav'), recursive=True):
-                        lfasr_model = lfasr.add_task(filename)
-                        batch_item_model = models.BatchItemModel(filename=filename, batch=batch_model, lfasr=lfasr_model)
-                        batch_item_model.save()
-                        print('New wav file upload:', filename)
-
-                    # Remove all temp files
-                    shutil.rmtree(tempdir)
+                        # Upload all wav file
+                        for filename in glob(path.join(tempdir, '**/*.wav'), recursive=True):
+                            lfasr_model = lfasr.add_task(filename)
+                            batch_item_model = models.BatchItemModel(filename=filename, batch=batch_model, lfasr=lfasr_model)
+                            batch_item_model.save()
+                            print('New wav file upload:', filename)
+                    finally:
+                        # Remove all temp files
+                        shutil.rmtree(tempdir)
 
             mail_model.save()
         except Exception as e:
             print(_, e)
 
 
+def send_mail(receivers, title, content, attachments=[], encoding='utf-8'):
+    """ """
+    smtpServer = config.smtp_address
+    smtpPort = config.smtp_port
+    smtpUser = config.smtp_user
+    smtpPassword = config.smtp_password
+    smtpSender = config.smtp_user
+
+    message = MIMEMultipart()
+    contentPart = MIMEText(content, _subtype='html', _charset=encoding)
+    message.attach(contentPart)
+
+    # 创建附件部分
+    for filename in attachments[::-1]:
+        # 获取不含路径的文件名
+        fn = path.split(filename)[-1]
+        attMimeText = MIMEText(open(filename, 'rb').read(), 'base64', encoding)
+        attMimeText["Content-Type"] = 'application/octet-stream'
+        attMimeText["Content-Disposition"] = u'attachment; filename="%s"' % header.Header(fn).encode(encoding)
+        message.attach(attMimeText)
+
+    # Add Header
+    message['to'] = ','.join(receivers)
+    message['from'] = smtpSender
+    message['subject'] = title
+
+    # Send it
+    smtp = smtplib.SMTP_SSL(smtpServer, smtpPort)
+    smtp.login(smtpUser, smtpPassword)
+    smtp.sendmail(smtpSender, receivers, message.as_string())
+    smtp.quit()
+
+    # 返回成功
+    return True
+
+
+def send_feedback():
+    """ """
+    for batch_model in models.BatchModel.objects.filter(state='processing'):
+        qs = batch_model.batchitemmodel_set.filter(~Q(lfasr__step='finish'))
+        if len(qs) == 0:
+            print('Send feedback for mail: %s' % batch_model.mail.message_id)
+            data_list = []
+            for item_model in batch_model.batchitemmodel_set.all():
+                lfasr = json.loads(item_model.lfasr.data)
+                data = OrderedDict()
+                data['filename'] = path.join(*item_model.filename.split('/')[2:])
+                data['lfasr'] = ','.join([seg['onebest'] for seg in lfasr])
+                data_list.append(data)
+            df = pd.DataFrame(data_list)
+
+            tempdir = tempfile.mkdtemp(dir='/tmp')
+            try:
+                # Set finish flag
+                batch_model.state = 'finish'
+                batch_model.save()
+
+                filename = path.join(tempdir, '结果.xls')
+                df.to_excel(filename)
+
+                receivers = [batch_model.mail.mailfrom]
+                title = '质检结果'
+                content = '''
+                触发邮件时间: %s
+                ''' % datetime.strftime(batch_model.mail.date, '%Y-%m-%d %H:%M:%S')
+                send_mail(receivers, title, content, [filename])
+
+            finally:
+                shutil.rmtree(tempdir)
+
+
 def main():
     """ """
     while True:
         read_mail()
-        sleep(1)
+        send_feedback()
+        sleep(5)
 
 
 if __name__ == "__main__":
